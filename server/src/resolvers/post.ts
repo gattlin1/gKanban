6105974abc6c6bc4e7870d49c835be5c03dc38fd
gattlin1 <gattlin1@live.missouristate.edu>
2022-06-03 15:34:02 -0400

    Adding vote status.

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -44,37 +44,52 @@ export class PostResolver {
   @Query(() => PaginatedPosts)
   async posts(
     @Arg('limit', () => Int) limit: number,
-    @Arg('cursor', () => String, { nullable: true }) cursor: string | null
+    @Arg('cursor', () => String, { nullable: true }) cursor: string | null,
+    @Ctx() { req }: MyContext
   ): Promise<PaginatedPosts> {
-    const realLimit = Math.min(50, limit) + 1;
-    const realLimitPlusOne = realLimit + 1;
+    // 20 -> 21
+    const realLimit = Math.min(50, limit);
+    const reaLimitPlusOne = realLimit + 1;
 
-    const replacements: any[] = [realLimitPlusOne];
+    const replacements: any[] = [reaLimitPlusOne];
+
+    if (req.session.userId) {
+      replacements.push(req.session.userId);
+    }
+
+    let cursorIdx = 3;
     if (cursor) {
       replacements.push(new Date(parseInt(cursor)));
+      cursorIdx = replacements.length;
     }
+
     const posts = await AppDataSource.query(
       `
-      select p.*,
-      json_build_object(
-        'id', u.id,
-        'username', u.username,
-        'email', u.email,
-        'createdAt', u."createdAt",
-        'updatedAt', u."updatedAt"
-        ) creator
-      from post p
-      inner join public.user u on u.id = p."creatorId"
-      ${cursor ? `where p."createdAt" < $2` : ''}
-      order by p."createdAt" DESC
-      limit $1
-    `,
+        select p.*,
+        json_build_object(
+          'id', u.id,
+          'username', u.username,
+          'email', u.email,
+          'createdAt', u."createdAt",
+          'updatedAt', u."updatedAt"
+          ) creator,
+        ${
+          req.session.userId
+            ? '(select value from updoot where "userId" = $2 and "postId" = p.id) "voteStatus"'
+            : 'null as "voteStatus"'
+        }
+        from post p
+        inner join public.user u on u.id = p."creatorId"
+        ${cursor ? `where p."createdAt" < $${cursorIdx}` : ''}
+        order by p."createdAt" DESC
+        limit $1
+        `,
       replacements
     );
 
     return {
       posts: posts.slice(0, realLimit),
-      hasMore: posts.length === realLimitPlusOne,
+      hasMore: posts.length === reaLimitPlusOne,
     };
   }
 
@@ -118,7 +133,7 @@ export class PostResolver {
   }
 
   @Mutation(() => Boolean)
-  //@UseMiddleware(isAuth)
+  @UseMiddleware(isAuth)
   async vote(
     @Arg('postId', () => Int) postId: number,
     @Arg('value', () => Int) value: number,
@@ -127,20 +142,50 @@ export class PostResolver {
     const { userId } = req.session;
     const isUpdoot = value > 0;
     const realValue = isUpdoot ? 1 : -1;
-    await AppDataSource.query(
-      `
-      START TRANSACTION;
+    const updoot = await Updoot.findOne({ where: { postId, userId } });
 
-      insert into updoot("userId", "postId", value)
-      values (${userId},${postId},${realValue});
+    // the user has voted on the post before and is changing their vote
+    if (updoot && updoot.value !== realValue) {
+      await AppDataSource.transaction(async (tm) => {
+        await tm.query(
+          `
+          update updoot
+          set value = $1
+          where "postId" = $2 and "userId" = $3
+          `,
+          [realValue, postId, userId]
+        );
 
-      update post
-      set points = points + ${realValue}
-      where id = ${postId};
+        await tm.query(
+          `
+          update post
+          set points = points + $1
+          where id = $2
+          `,
+          [2 * realValue, postId]
+        );
+      });
+    } else if (!updoot) {
+      // has never voted before
+      await AppDataSource.transaction(async (tm) => {
+        await tm.query(
+          `
+          insert into updoot("userId", "postId", value)
+          values ($1,$2,$3);
+          `,
+          [userId, postId, realValue]
+        );
+        await tm.query(
+          `
+          update post
+          set points = points + $1
+          where id = $2
+          `,
+          [realValue, postId]
+        );
+      });
+    }
 
-      COMMIT;
-    `
-    );
     return true;
   }
 }
