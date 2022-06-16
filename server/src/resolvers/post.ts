14e9cf5cd15557ac1f247467c805b853af623ce4
gattlin1 <gattlin1@live.missouristate.edu>
2022-06-16 09:09:24 -0400

    Only allow users that own a post to delete them.

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -123,9 +123,13 @@ export class PostResolver {
   }
 
   @Mutation(() => Boolean)
-  async deletePost(@Arg('id') id: number): Promise<boolean> {
+  @UseMiddleware(isAuth)
+  async deletePost(
+    @Arg('id') id: number,
+    @Ctx() { req }: MyContext
+  ): Promise<boolean> {
     try {
-      await Post.delete(id);
+      await Post.delete({ id, creatorId: req.session.userId });
       return true;
     } catch {
       return false;
